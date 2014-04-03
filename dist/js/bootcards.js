bd833f2562785b79abad8523e3a2e30c166fbaa7
Mark Leusink <m.leusink@gmail.com>
2014-04-03 13:25:43 +0200

    function added to JavaScript to disable rubber banding on iOS

--- dist/js/bootcards.js
+++ dist/js/bootcards.js
@@ -1,8 +1,9 @@
 var bootcards = bootcards || {};
 
+bootcards._isXS = null;
+
 /*
  * Get the Bootstrap enviroment we're in.
-
  * Found at http://stackoverflow.com/questions/14441456/how-to-detect-which-device-view-youre-on-using-twitter-bootstrap-api/15150381#15150381
  * Function writen by Raphael_ (http://stackoverflow.com/users/1661358/raphael)
  */
@@ -24,14 +25,37 @@ bootcards.findBootstrapEnvironment = function() {
     };
 }
 
-$(document).ready( function() {
+bootcards.isXS = function() {
+    if (this._isXS === null ) {
+        this._isXS = (this.findBootstrapEnvironment() == "ExtraSmall");
+    }
+    return this._isXS;
+}
 
-	//enable the slide in menu
-    $('.offcanvas').offcanvas({
-    	toggle : false
+/*
+ * Disable rubberbanding effect in iOS.
+ * Based on the 'Baking Soda Paste' technique written by Armagan Amcalar at
+ * http://blog.armaganamcalar.com/post/70847348271/baking-soda-paste
+ */
+bootcards.disableRubberBanding = function() {
+   document.body.addEventListener('touchstart', function() {
+        document.body.addEventListener('touchmove', function moveListener(e) {
+            document.body.removeEventListener('touchmove', moveListener);
+
+            var el = e.target;
+
+            do {
+
+                var h = parseInt(window.getComputedStyle(el, null).height, 10);
+                var sH = el.scrollHeight;
+
+                if (h < sH) {
+                    return;
+                }
+            } while (el != document.body && el.parentElement != document.body && (el = el.parentElement));
+
+            e.preventDefault();
+        });
     });
-    $('.offcanvas-toggle').on('click', function() {
-    	$('.offcanvas').offcanvas('toggle');
-    })
-   
-});
+
+}
