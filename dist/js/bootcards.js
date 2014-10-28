926d676dfba22ca58a69314d6668720c0b3ecc78
Mark Leusink <m.leusink@gmail.com>
2014-10-28 19:54:18 +0100

    dist folders built for v1.0

--- dist/js/bootcards.js
+++ dist/js/bootcards.js
@@ -55,34 +55,17 @@ bootcards.init = function( _options ) {
 
 };
 
-
-/*
- * Get the Bootstrap enviroment we're in.
- * Found at http://stackoverflow.com/questions/14441456/how-to-detect-which-device-view-youre-on-using-twitter-bootstrap-api/15150381#15150381
- * Function writen by Raphael_ (http://stackoverflow.com/users/1661358/raphael)
- */
-bootcards.findBootstrapEnvironment = function() {
-    var envs = ["ExtraSmall", "Small", "Medium", "Large"];
-    var envValues = ["xs", "sm", "md", "lg"];
-
-    $el = $('<div>');
-    $el.appendTo($('body'));
-
-    for (var i = envValues.length - 1; i >= 0; i--) {
-        var envVal = envValues[i];
-
-        $el.addClass('hidden-'+envVal);
-        if ($el.is(':hidden')) {
-            $el.remove();
-            return envs[i];
-        }
-    }
-};
-
 bootcards.isXS = function() {
+
     if (this._isXS === null ) {
-        this._isXS = (this.findBootstrapEnvironment() == "ExtraSmall");
+
+        //check if we're in the Bootstrap XS environment
+        var $check = $("<div class='visible-xs'>").appendTo($("body"));
+        this._isXS = $check.is(":visible");
+        $check.remove();
+
     }
+
     return this._isXS;
 };
 
